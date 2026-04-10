4789013828f13cdc7b44433cb4bdfb48cb29b489
Mazinho Developer <mazinhodeveloper@gmail.com>
2026-04-09 22:04:54 -0300

    Updating Configurarions - DBs.sql

--- Configurations/DB-03.sql
+++ Configurations/DB-03.sql
@@ -1,48 +1,32 @@
-USE master; 
-GO 
+-- 1. Usando o banco de dados
+USE APIASPDOTNETMSSQLServerDB;
+GO
 
--- Criando o banco de dados 
-CREATE DATABASE [APIASPDOTNETMSSQLServerDB];
-GO 
+-- 2. Selecionando a tabela ztACL
+SELECT * FROM ztACL;
 
-USE APIASPDOTNETMSSQLServerDB; 
-GO 
+-- # 3. Inserir dado na tabela ztACL
+INSERT INTO ztACL (tipo, descricao)
+    VALUES
+    ('Master', 'Acesso total ao sistema, especifica para o administrador do sistema.');
+GO
 
--- 2. Criar as tabelas de dependencias 
-CREATE TABLE ztACL ( 
-    idACL INT IDENTITY(1,1) PRIMARY KEY, 
-    tipo VARCHAR(20) UNIQUE NOT NULL, -- ex: Master, Admin, Cliente, Funcionario 
-    descricao VARCHAR(100) NOT NULL 
-); 
+-- # 3. Inserir multiplos dados na tabela ztACL
+-- URL: http://localhost:8080/swagger
+-- POST: /api/acl
+{
+  "tipo": "Desenvolvedor",
+  "descricao": "Acesso de desenvolvimento, para suporte tecnico."
+}
 
-
-SELECT * FROM ztACL; 
-
--- # 2. Inserir dado na tabela ztACL 
-INSERT INTO ztACL (tipo, descricao) 
-    VALUES 
-    ('Master', 'Acesso total ao sistema, especifica para o administrador do sistema.'); 
-GO 
-
--- # 3. Inserir multiplos dados na tabela ztACL 
-MERGE INTO ztACL AS Target 
-    USING (
-        VALUES 
-        ('Administrador', 'Acesso administrativo, para suporte tecnico.'), 
-        ('Funcionário', 'Acesso ao sistema para gestão.'), 
-        ('Cliente', 'Acesso ao sistema para uso.') 
-    ) AS Source (tipo, descricao) 
-    ON Target.tipo = Source.tipo -- Verifica se o tipo já existe 
-    -- Se o 'tipo' não existir, insere apenas tipo e descricao (id é automático) 
-    WHEN NOT MATCHED BY TARGET THEN 
-        INSERT (tipo, descricao) 
-        VALUES 
-        (Source.tipo, Source.descricao) 
-    WHEN MATCHED THEN 
-        UPDATE SET Target.descricao = Source.descricao; 
-GO 
-
-	
+-- # 4. Inserir multiplos dados na tabela ztACL
+-- URL: http://localhost:8080/swagger
+-- POST: /api/acl
+{
+  "tipo": "Convidado",
+  "descricao": "Acesso ao sistema para uso."
+}
 
 
-
+-- 5. Selecionando a tabela ztACL
+SELECT * FROM ztACL;
